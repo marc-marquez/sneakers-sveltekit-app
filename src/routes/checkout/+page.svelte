556af314723e6cbdb779ed611d0ccf4bdfd3a0b5
Marc Marquez <marc.marquez@gmail.com>
2024-05-08 16:15:06 -0700

    Add some formatting to checkout.

--- src/routes/checkout/+page.svelte
+++ src/routes/checkout/+page.svelte
@@ -2,24 +2,34 @@
     import PageLayout from "../../shared/PageLayout.svelte";
     import CartItems from "../../shared/CartItems.svelte";
 
-    let fields = ['name', 'email', 'address', 'city', 'state', 'zip', 'country'];
+    let personFields = ['name', 'email', 'address', 'city', 'state', 'zip', 'country'];
+    let creditFields = ['number', 'month', 'year', 'cvv'];
 </script>
 
 <PageLayout>
     <div class="checkout">
         <h1>Checkout</h1>
         <CartItems />
-        <div class="box">
-            <form>
-                {#each fields as field}
+        <form>
+            <div class="box">
+                {#each personFields as field}
                     <div style="display: flex; justify-content: space-between;">
                         <label for={field}>{field}</label>
                         <input style="margin-left: 20px;" type="text" id={field} name={field} required>
                     </div>
                 {/each}
-                <button type="submit">Submit</button>
-            </form>
-        </div>
+            </div>
+            <div class="box">
+                {#each creditFields as field}
+                    <div style="display: flex; justify-content: space-between;">
+                        <label for={field}>{field}</label>
+                        <input style="margin-left: 20px;" type="text" id={field} name={field} required>
+                    </div>
+                {/each}
+            </div>
+            
+        </form>
+        <button type="submit">Submit</button>
     </div>
 </PageLayout>
 
@@ -34,13 +44,9 @@
     .box {
         display: grid;
         grid-template-columns: 1fr;
-        border: 1px solid black;
-        border-radius: 10px;
-        flex: 1;
-        width: 100%;
-        place-items: center;
+        place-items: flex-end;
         padding: 20px;
-        background-color: #fafafa
+        align-items: start;
     }
 
     label, input {
@@ -51,8 +57,13 @@
 
     form {
         display: grid;
+        grid-template-columns: 1fr 1fr;
         grid-auto-flow: row;
-        grid-gap: 10px;
+        grid-gap: 20px;
+        border: 1px solid darkgrey;
+        background-color: lightgrey;
+        border-radius: 50px;
+        margin-bottom: 20px;
     }
 
     button {
